cc93923b3be7d9869a5d144980c6bc0e956f4053
abdelkhalekalashker <abdelkhalekalashker@gmail.com>
2022-04-28 16:13:34 +0200

    fix listing of job posts using ransack

--- job_seeker/app/controllers/search_controller.rb
+++ job_seeker/app/controllers/search_controller.rb
@@ -1,6 +1,13 @@
 class SearchController < ApplicationController
-  def index
-    @query = Post.ransack(params[:q])
-    @posts = @query.result
-  end
+#   def index
+#     @q = Post.ransack(params[:q])
+#     @posts = @q.result
+#   end
+# end
+# before_filter :index
+
+def index 
+  @query = Post.ransack(params[:q])
+  @posts = @query.result
+  
 end
